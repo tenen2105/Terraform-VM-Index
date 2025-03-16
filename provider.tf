3096ea8fb56bf541438aa30eb2824d7f824dfaa4
Eddie Tenen <tenen21@gmail.com>
2025-03-16 14:55:04 -0400

    Update provider.tf
    removed private information.

--- provider.tf
+++ provider.tf
@@ -9,8 +9,5 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  client_id = "f8bd6e39-5080-42e7-9cff-a0c900d17112"
-  client_secret = "mjF8Q~ESZUAamHGdiFPGYposQ4LUg4X-ZG1debLE"
-  tenant_id = "b0ba5a5b-3f5b-44ff-86eb-1b3bac353f7f"
-  subscription_id = "e3740935-57ad-46de-91b7-3705809f1a28"
+
 }
